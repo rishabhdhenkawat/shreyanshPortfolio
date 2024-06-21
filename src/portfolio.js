51fc8b855564de4cc05e8667f57979ca0ae2d9ac
rishabhdhenkawat <44580998+rishabhdhenkawat@users.noreply.github.com>
2024-06-21 18:51:41 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -354,6 +354,7 @@ const resumeSection = {
   title: "Resume",
   subtitle: "Feel free to download my resume",
   // Please Provide with Your Resume
+  resumeLink: require("./assets/resume_.pdf"),
   display: true // Set false to hide this section, defaults to true
 };
 
